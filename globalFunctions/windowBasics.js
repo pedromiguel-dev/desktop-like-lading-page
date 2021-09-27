72d924e513f7a0cf61a146ba81aea5157b8bf4de
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2021-09-27 12:22:52 -0300

    reorganizado
    implementado export function para melhhor oroganização do código

--- globalFunctions/windowBasics.js
+++ globalFunctions/windowBasics.js
@@ -1,15 +1,15 @@
-//criar janela
 let janelas = document.getElementsByClassName("window");
-
-function construirJanela() {
-  const container = document.querySelector("body");
-  let Janela = document.getElementsByTagName("template")[0];
-  let novaJanela = Janela.content.cloneNode(true);
-  container.appendChild(novaJanela);
+//criar janela
+function construirJanela(e) {
+  if (e.target.id == "app") {
+    const container = document.querySelector("body");
+    let Janela = document.getElementsByTagName("template")[0];
+    let novaJanela = Janela.content.cloneNode(true);
+    container.appendChild(novaJanela);
+  }
 }
-
-//mover janelas
-document.addEventListener("mousedown", (e) => {
+//mover janela
+function moverJanela(e) {
   if (e.target.nodeName == "HEADER") {
     var target = e.target;
     target.addEventListener("mousemove", onDrag);
@@ -25,8 +25,7 @@ document.addEventListener("mousedown", (e) => {
   document.addEventListener("mouseup", () => {
     target.removeEventListener("mousemove", onDrag);
   });
-});
-
+}
 function onDrag({ movementX, movementY }) {
   let parent = this.parentElement;
   let getStyle = window.getComputedStyle(parent);
@@ -36,25 +35,23 @@ function onDrag({ movementX, movementY }) {
   parent.style.left = `${leftVal + movementX}px`;
   parent.style.top = `${topVal + movementY}px`;
 }
-
 //fechar janelas
-document.addEventListener("click", (e) => {
-  console.log(e.target.id);
+function fecharJanela(e) {
   if (e.target.id == "fechar") {
     e.target.parentElement.parentElement.parentElement.remove();
   }
-});
-
+}
 ///minimizar janelas
-document.addEventListener("click", (e) => {
+
+function minimizarJanela(e) {
   if (e.target.id == "min") {
     e.target.parentElement.parentElement.parentElement.style.opacity = "0";
     e.target.parentElement.parentElement.parentElement.style.pointerEvents =
       "none";
   }
-});
-//max janelas
-document.addEventListener("click", (e) => {
+}
+//maximizar janelas
+function maxJanela(e) {
   if (e.target.id == "max") {
     let janela = e.target.parentElement.parentElement;
     if (janela.parentElement.id == "maximized") {
@@ -75,4 +72,12 @@ document.addEventListener("click", (e) => {
     janela.parentElement.style.borderRadius = "0";
     janela.parentElement.id = "maximized";
   }
-});
+}
+
+export {
+  construirJanela,
+  moverJanela,
+  fecharJanela,
+  minimizarJanela,
+  maxJanela,
+};
---
+++ main.js
@@ -0,0 +1,7 @@
+import * as Janelas from "./globalFunctions/windowBasics.js";
+
+document.addEventListener("click", Janelas.construirJanela);
+document.addEventListener("mousedown", Janelas.moverJanela);
+document.addEventListener("click", Janelas.maxJanela);
+document.addEventListener("click", Janelas.fecharJanela);
+document.addEventListener("click", Janelas.minimizarJanela);
